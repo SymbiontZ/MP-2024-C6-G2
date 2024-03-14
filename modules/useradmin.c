961cccb4e989f746410e72a01ac59c3617cc716b
FabioR <fabiorrojas56@gmail.com>
2024-03-14 17:54:24 +0100

    FRR v1

--- modules/useradmin.c
+++ modules/useradmin.c
@@ -7,8 +7,9 @@ void excepcion (int, char[]);
 
 clients cargar_clientes(){
     char filename[] = "../data/Clientes.txt";
-    int n_clients = 0;                          //Numero de clientes registrados
-    char cad_linea[170];                        //Caracteres maximos que puede ocupar una linea en fichero
+    int n_clients = 0,i;                          //Numero de clientes registrados
+    char cad_linea[170]; 
+    int campo_cliente;                      //Caracteres maximos que puede ocupar una linea en fichero
 
     
     FILE *f_clients;
@@ -22,6 +23,7 @@ clients cargar_clientes(){
     
     while(fgets(cad_linea, sizeof(cad_linea), f_clients)){
         n_clients++;
+        printf("%s", cad_linea);
     }
     rewind(f_clients);                                          //Necesario para volver a leer el fichero
     
@@ -30,10 +32,18 @@ clients cargar_clientes(){
     C.n_clients = n_clients;
     C.clients = malloc(n_clients * sizeof(client));
     
+    //BUCLE PARA RELLENAR LA ESTRUCTURA DE CLIENTES//
+    for (i = 0; i < n_clients; i++) {
+        while(fgets(cad_linea, sizeof(cad_linea), f_clients)){
+            campo_cliente = sscanf(cad_linea, "%d");
+        }
+        
+    }
+    printf("%s", C.clients[0].email);
 
     fclose(f_clients);
     return C;
-} 
+}
 
 
 
