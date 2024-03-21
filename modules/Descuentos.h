e3e21dee0adb357012f54df1af5663b1b7c30523
Juan Manuel Caucelo Rodríguez <126818557+JUwUanma@users.noreply.github.com>
2024-03-21 17:10:48 +0100

    JMCR v1.1

--- modules/Descuentos.h
+++ modules/Descuentos.h
@@ -1,13 +1,16 @@
 #ifndef DESCUENTOS_H_INCLUDED
 #define DESCUENTOS_H_INCLUDED
 
+#define MAX_DESC 87
+#define MAX_DESCLI 42
+
 typedef struct {
 
-    char Id_cod[11],                //Identificador del cï¿½digo promocional
-         Descrip[51],               //Descripciï¿½n del cï¿½digo promocional
-         Tipo[8],                  //"codpro" (cï¿½digo promocional) o "cheqreg" (cheque regalo)
+    char Id_cod[11],                //Identificador del código promocional
+         Descrip[51],               //Descripción del código promocional
+         Tipo[8],                  //"codpro" (código promocional) o "cheqreg" (cheque regalo)
          Estado[9],                 //Estado del cheque ("acctivo" o "inactivo")
-         Aplicable[7];              //"todos" (si es aplicable a todos los productos) o "esizon" (si sï¿½lo esaplicable a los gestionados por ESIZON)
+         Aplicable[7];              //"todos" (si es aplicable a todos los productos) o "esizon" (si sólo esaplicable a los gestionados por ESIZON)
 
     int  Importe;                   //Importe del descuento en euros
 
@@ -15,16 +18,16 @@ typedef struct {
 
 typedef struct {
 
-    char Id_cod[11];                //Identificador del cï¿½digo promocional
+    char Id_cod[11];                //Identificador del código promocional
 
     int  Id_cliente,                //Identificador del cliente poseedor del descuento
-         dia_asig,                  //Dï¿½a de la fecha de asignaciï¿½n
-         mes_asig,                  //Mes de la fecha de asignaciï¿½n
-         anio_asig,                 //Aï¿½o de la fecha de asignaciï¿½n
-         dia_cad,                   //Dï¿½a de la fecha de caducidad
+         dia_asig,                  //Día de la fecha de asignación
+         mes_asig,                  //Mes de la fecha de asignación
+         anio_asig,                 //Año de la fecha de asignación
+         dia_cad,                   //Día de la fecha de caducidad
          mes_cad,                   //Mes de la fecha de caducidad
-         anio_cad,                  //Aï¿½o de la fecha de caducidad
-         Estado;                    //El valor 0 indica que el cupï¿½n no ha sido aplicado y el valor 1 que si lo ha sido
+         anio_cad,                  //Año de la fecha de caducidad
+         Estado;                    //El valor 0 indica que el cupón no ha sido aplicado y el valor 1 que si lo ha sido
 
 }DescuentosClientes;
 
@@ -32,21 +35,28 @@ typedef struct {
 
 typedef struct{
 
-    int tam;                        //Tamaï¿½o del vector
+    int tam;                        //Tamaño del vector
     Descuentos *Desc;               //Vector de tipo Descuentos
 
 }Vect_Desc;
 
 typedef struct{
 
-    int tam;                        //Tamaï¿½o del vector
+    int tam;                        //Tamaño del vector
     DescuentosClientes *DescCli;    //Vector de tipo DescuentosClientes
 
 }Vect_DescCli;
 
 
-Vect_Desc Cargar_Descuentos();      //Cabecera
-                                    //Precondiciï¿½n: El fichero Descuentos.txt debe estar relleno
-                                    //Postcondiciï¿½n: Devuelve una estructura con el vector de estructuras Desc y almacena su tamaï¿½o
+//Precondición: El fichero Descuentos.txt debe estar relleno
+//Postcondición: Devuelve una estructura con el vector de estructuras Desc y almacena su tamaño
+void Cargar_Descuentos();
+
+
+//Precondición: El fichero DescuentosClientes.txt debe estar relleno
+//Postcondición: Devuelve una estructura con el vector de estructuras Desc y almacena su tamaño
+void Cargar_DescuentosClientes();
+
+
 
 #endif // DESCUENTOS_H_INCLUDED
