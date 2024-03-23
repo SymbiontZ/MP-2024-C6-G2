d9e8dc82bec569ede9b63695fc9507e16458d295
FabioR <fabiorrojas56@gmail.com>
2024-03-23 20:25:18 +0100

    FRR v2.5_1

--- modules/usermain.c
+++ modules/usermain.c
@@ -5,8 +5,7 @@ int main(){
     printf("###INTERFAZ INICIAL###\n");
     
     clients C = cargar_clientes();
-    //C = agregar_cliente(C);
-    //guardar_clientes(C);
+    C = agregar_cliente(C);
     gestionar_cliente(C, C.n_clients-1);
     return 0;
 }
