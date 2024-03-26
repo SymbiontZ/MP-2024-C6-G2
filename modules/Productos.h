7248f66fe7bb8f1663428bcead33112e6331ff54
Adrián <127325984+RodilloCagable2@users.noreply.github.com>
2024-03-26 19:10:09 +0100

    AGBv2

--- modules/Productos.h
+++ modules/Productos.h
@@ -5,18 +5,24 @@
 
 typedef struct {
 	int id_prod;		//Identificador del producto
+	char nombre[16];	//Nombre del producto
 	char descrip[51];	//Descripción del producto
 	int id_categ;		//Id de la categoría a la que pertenece el producto
 	int id_gestor;		//Id del gestor del producto
 	int stock;			//Stock del producto
 	int entrega;		//Fecha de compromiso de entrega en días
 	int importe;		//Importe del producto en euros
-	int lleno;			//Variable utilizada para diferenciar los productos con todos sus valores rellenados
 }productos;
 
-void vacio (productos []);
+typedef struct {
+	productos *produ;
+	int num_prod;
+}produ_vect;
+
+produ_vect cargar_productos ();
 void cambio (char []);
-void rellenar_produ (productos []);
-void mostrar_produ (productos []);
+void menu_prod ();
+void rellenar_produ (productos *);
+void mostrar_produ (productos *);
 
 #endif
