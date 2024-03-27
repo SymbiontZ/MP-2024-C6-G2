726ef1a91d5a35b421c288c92a021fbfa3364970
FabioR <fabiorrojas56@gmail.com>
2024-03-27 14:15:11 +0100

    FRR FLUJO BASICO v1

--- modules/main.c
+++ modules/main.c
@@ -4,25 +4,25 @@
 #include"useradmin.h"
 #include"complementos.h"
 
-void iniciar_sesion_email();
+void inicsesion_email();
 void iniciar_sesion_psw(int , int );
 
 int main(){
     clear();
-    iniciar_sesion_email();
+    inicsesion_email();
 
     return 0;
 }
 
-void iniciar_sesion_email(){
+void inicsesion_email(){
     admin_prov_vect adminprov = cargar_adminprov();
     transport_vect transport = cargar_transportistas();
     clients client = cargar_clientes();
 
     char cad_email[MAX_EMAIL], cad_pasw[MAX_PSW];
     int len, i;
-    int mode = 0;               //Modo de inicio sesion: | 1 CLIENTE | 2 ADMIN/PROVEEDOR | 3 TRANSPORTISTA
-    int id;                 //Guarda la posicion relacionado con email
+    int verif = 0;                  //Comprueba existe email | 0 -> NO EXISTE / 1 -> EXISTE
+    int pos;                         //Guarda la posicion relacionado con email
 
     //INTRDUCIR CORREO//
     printf("\nIntroduzca el correo: ");
@@ -34,29 +34,40 @@ void iniciar_sesion_email(){
     //COMPROBAR EN USUARIOS
     for(i = 0; i < client.n_clients; i++){
         if(strcmp(cad_email, client.clients[i].email) == 0){
-            id = i;
-            mode = 1;
-            iniciar_sesion_psw(id, mode);
+            pos = i;
+            verif = 1;
+            inicsesion_cliente(pos);
         }
     }
+    //COMPROBAR EN ADMIN/PROV
     for(i = 0; i < adminprov.tam; i++){
         if(strcmp(cad_email, adminprov.usuarios[i].email) == 0){
-            id = 1;
-            mode = 2;
-            iniciar_sesion_psw(id, mode);
+            pos = i;
+            
+            if(strcmp(adminprov.usuarios[pos].Perfil_usuario, "proveedor") == 0)             //ES PROV
+                inicsesion_prov(pos);    
+            else if(strcmp(adminprov.usuarios[pos].Perfil_usuario, "administrador") == 0)    //ES ADMIN
+                inicsesion_admin(pos);
+            else{
+                printf("No se pudo encontrar prefil de usuario");
+                getchar();
+                exit(EXIT_FAILURE);
+            }
+            verif = 1;
         }
     }
+    //COMPROBAR EN TRANSPORTISTAS
     for(i = 0; i<transport.tam;i++){
         if(strcmp(cad_email, transport.transportistas[i].email) == 0){
-            id = i;
-            mode = 3;
-            iniciar_sesion_psw(id, mode);
+            pos = i;
+            inicsesion_transport(pos);
+            verif = 1;
         }
     }
 
     //AGREGAR USUARIO//
     char opt = '0';
-    if(mode == 0){
+    if(verif == 0){
         printf("No se ha encontrado ese correo, desea registrarse como cliente[s/n]: ");
         scanf(" %c", &opt);
         while(opt != 's' && opt != 'n'){
@@ -74,7 +85,7 @@ void iniciar_sesion_email(){
 void iniciar_sesion_psw(int id, int mode){
     if(mode == 1){
         clients c = cargar_clientes();
-        printf("perfil cliente\n");
+        
 
     }else if(mode == 2){
         admin_prov_vect adminprov = cargar_adminprov();
